fefeaaa9c82084ce9a46efce554f8860b1cd06a4
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2023-11-19 12:42:24 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/252361.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "252361"
+  "252361.pdb"
+  "CMakeFiles/252361.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/252361.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Project 0!\n");
+    return 0;
+}
+##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
